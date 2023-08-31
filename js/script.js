887ec2119d6f0647390b8598c246cc5e20c908f5
Tisca <t.scaffidi@hotmail.com>
2023-08-31 15:28:07 +0200

    Continuation of Potluck Guest List with assigned dishes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-// invite button
 const addGuestButton = document.querySelector(".invite");
-// label for the invite button
 const guestInputLabel = document.querySelector(".add-guest label");
-// text input box
 const guestInput = document.querySelector(".add-guest input");
-// unordered list (not yet visible)
 const guestList = document.querySelector(".guest-list");
-// span class for number of guests attending
 const guestCount = document.querySelector(".attendance");
-// alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items");
+
 
 addGuestButton.addEventListener("click", function () {
     const guest = guestInput.value;
@@ -42,3 +39,36 @@ const updateGuestcount = function () {
         guestFull.classList.remove("hide");
     }
 }
+
+const assignItems = function () {
+    const potluckItems = [
+        "mango-mozzarella-salad",
+        "chips",
+        "bread",
+        "cake",
+        "fresh fruits",
+        "icecream",
+        "cookies",
+        "chicken wings",
+        "cut vegetables",
+        "dips and sauces",
+        "ice tea",
+        "water"
+    ];
+
+    const allGuests = document.querySelectorAll(".guest-list li");
+
+    for (let guest of allGuests) {
+        let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+        assignedItems.append(listItem);
+        potluckItems.splice(randomPotluckIndex, 1);
+    }
+};
+
+assignButton.addEventListener("click", function(){
+    assignItems();
+    assignButton.disabled = true;
+});
